699bac60545e665aa267e3e267c4069a4ea94da5
Aravinda <aravindaweerasekara@gmail.com>
2016-08-02 07:47:34 +0530

    webdriver update

--- src/main/java/com/hms/app/SelectDropDownMenu.java
+++ src/main/java/com/hms/app/SelectDropDownMenu.java
@@ -5,11 +5,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
+
 /**
  * Created by Aravinda on 02-Aug-16.
  */
 public class SelectDropDownMenu {
     public static void selectDDM(){
+        File path = new File("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
+        System.setProperty("webdriver.chrome.driver", path.getAbsolutePath());
         WebDriver webDriver = new ChromeDriver();
         webDriver.get("http://www.w3schools.com/howto/howto_css_dropdown.asp");
         WebElement ddm;
